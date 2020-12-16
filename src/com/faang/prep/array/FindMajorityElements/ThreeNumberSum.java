7d935efb71816051ebedffb4f39661cc1244d795
Eomaxl <souravsnigdh@gmail.com>
2020-12-16 22:07:06 +0530

    Initial Commit

---
+++ src/com/faang/prep/array/FindMajorityElements/ThreeNumberSum.java
@@ -0,0 +1,23 @@
+package com.faang.prep.array.FindMajorityElements;
+import java.util.*;
+/**
+ *  Write a function that takes in an non-empty array of distinct integers
+ *  and an integer representing a target sum. The function should find all
+ *  triplets in the array that sum up to the target sum and return a two dimensional
+ *  array of all these triplets. The numbers in each triplet should be ordered
+ *  in ascending order, and the triplets themselves should be ordered in ascending
+ *  order with respect to the numbers they hold.
+ *  If no three numbers sum up to the target sum, the function should return
+ *  an empty array.
+ *
+ *  Sample Input: [12, 3, 1, 2, -6, 5, -8, 6] , target sum = 0;
+ *  Sample Output: [[-8, 2, 6], [-8, 3, 5]. [-6, 1, 5]]
+ *
+ *  Optimal Space and Time Complexity: O(n^2) time | O(n) where n is
+ *  the length of the input array.
+ * */
+public class ThreeNumberSum {
+    public static void main(String[] args) {
+
+    }
+}
