285cef0d953d5218394dbcde30f78b8d8cde7b44
Eomaxl <souravsnigdh@gmail.com>
2020-12-21 23:14:48 +0530

    ThreeNumberSum

--- src/com/faang/prep/array/FindMajorityElements/ThreeNumberSum.java
+++ src/com/faang/prep/array/FindMajorityElements/ThreeNumberSum.java
@@ -18,6 +18,39 @@ import java.util.*;
  * */
 public class ThreeNumberSum {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        System.out.println("Enter the target value ");
+        int target = sc.nextInt();
+        System.out.println("Enter the array element :");
+        for(int i = 0; i< size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        List<Integer[]> res = threeNumberSum(arr,target);
+    }
 
+    public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+        Arrays.sort(array);
+        List<Integer[]> triplets = new ArrayList<Integer[]>();
+            for(int i=0; i<array.length; i++){
+                int left = i + 1;
+                int right = array.length - 1;
+                while( left < right) {
+                    int currentSum = array[i] + array[left] + array[right];
+                    if( currentSum == targetSum) {
+                        Integer[] newTriplets = {array[i], array[left], array[right]};
+                        triplets.add(newTriplets);
+                        left++;
+                        right--;
+                    } else if (currentSum < targetSum) {
+                        left++;
+                    } else if (currentSum > targetSum) {
+                        right--;
+                    }
+                }
+            }
+        return triplets;
     }
 }
